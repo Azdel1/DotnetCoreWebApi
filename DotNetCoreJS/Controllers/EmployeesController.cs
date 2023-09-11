486f822c1238983c5986828001d3b8f023d22560
Abdul Azees <abdulazdel@gmail.com>
2023-09-11 19:40:54 +0530

    Auto release check

--- DotNetCoreJS/Controllers/EmployeesController.cs
+++ DotNetCoreJS/Controllers/EmployeesController.cs
@@ -21,6 +21,7 @@ namespace DotNetCoreJS.Controllers
             _context = context;
         }
 
+        //Get
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
         {
